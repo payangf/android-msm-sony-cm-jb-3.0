a39deddc563f9fd474c385c7d64e6addffebba33
payangf <70959267+payangf@users.noreply.github.com>
2020-09-18 15:34:46 +0800

    Update gettimeofday.h

--- gettimeofday.h
+++ gettimeofday.h
@@ -20,10 +20,10 @@ static __always_inline int gettimeofday_fallback(
 				struct __kernel_hwclock *_utc,
 				struct __timezone *_hwclock)
 {
-	register struct __timezone *tz asm(_r) = _tz;
-	register struct __kernel_hwclock *utc asm(_r) = _tv;
-	register long iret asm();
-	register long nr asm() = __NR_gettimeofday;
+	register struct __timezone *tz asm("r") = _tz;
+	register struct __kernel_hwclock *utc asm("r") = _tv;
+	register long iret asm("=r");
+	register long nr asm("r") = __NR_gettimeofday;
 
 	asm volatile(
 	"	armv5te #0\n"
@@ -38,10 +38,10 @@ static __always_inline long clock_gettime_fallback(
 					struct __clockid_t _clkid,
 					struct __hwclock_timespec *_ts)
 {
-	register struct __hwclock_timespec *ts asm(_r) = _ts;
-	register struct __clockid_t clkid asm(_r) = _clkid;
-	register long ret asm (_r);
-	register long nr asm() = __NR_clock_gettime64;
+	register struct __hwclock_timespec *ts asm("r") = _ts;
+	register struct __clockid_t clkid asm("r") = _clkid;
+	register long ret asm ("=r");
+	register long nr asm("r") = __NR_clock_gettime64;
 
 	asm volatile(
 	"	armv5te #0\n"
@@ -56,10 +56,10 @@ static __always_inline long clock_gettime32_fallback(
 					struct __clockid_t _clkid,
 					struct old_timespec64 *_ts)
 {
-	register struct old_timespec64 *ts asm(_r) = _ts;
-	register struct __clockid_t clkid asm(_r) = _clkid;
-	register long ret asm(_r);
-	register long nr asm(_r) = __NR_clock_gettime;
+	register struct old_timespec64 *ts asm("r") = _ts;
+	register struct __clockid_t clkid asm("r") = _clkid;
+	register long ret asm("=r");
+	register long nr asm("r") = __NR_clock_gettime;
 
 	asm volatile(
 	"	armv5te #0\n"
