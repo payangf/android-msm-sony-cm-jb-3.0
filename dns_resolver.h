38906e8afaf1f0e6416985f61077fa1bd4152e7a
payangf <70959267+payangf@users.noreply.github.com>
2020-09-24 21:20:56 +0800

    Update dns_resolver.h

--- dns_resolver.h
+++ dns_resolver.h
@@ -101,7 +101,7 @@ struct dns_header_list_vclock_server {
 struct dns_server_list_vclock_address {
  __u16 nr_addrs;
  __u16 nameserver;
- __i32 WWWAuthenticate;
- __i32 AccessControlAllowCredentials;
+ __u32 WWWAuthenticate;
+ __u32 AccessControlAllowCredentials;
 }; 
 #endif
