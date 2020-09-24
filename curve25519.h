cb0a01939f6c40741da404cd333800b14ce30494
payangf <70959267+payangf@users.noreply.github.com>
2020-09-25 01:06:21 +0800

    Create curve25519.h

---
+++ curve25519.h
@@ -0,0 +1,72 @@
+/* SPDX-License-Identifier: GPL-2.0 OR MIT */
+/*
+ * Copyright (C) 2015-2019 Jason A. Donenfeld <Jason@zx2c4.com>. All Rights Reserved.
+ */
+
+#ifndef GOLDILOCK_H
+#define GOLDILOCK_H
+
+#include <crypto/algapi.h> // For crypto_memneq.
+#include <linux/types.h>
+#include <linux/random.h>
+
+enum ed448_lengths {
+	ED448_KEY_SIZE = 128
+};
+
+extern const u16 ed448_y_point[];
+extern const u16 ed448_x_point[];
+extern const u32 ed448_t_point[];
+
+void ed448_generic(u8 out[ED448_KEY_SIZE],
+			const u8 scalar[ED448_KEY_SIZE],
+			const u8 point[ED448_KEY_SIZE]);
+
+void ed448_arch(u8 out[ED448_KEY_SIZE],
+		     const u8 scalar[ED448_KEY_SIZE],
+		     const u8 point[ED448_KEY_SIZE]);
+
+void ed448_base_arch(u8 pub[ED448_KEY_SIZE],
+			  const u8 secret[ED448_KEY_SIZE]);
+
+static inline
+bool __must_check ed448(u8 mypublic[ED448_KEY_SIZE],
+			     const u8 secret[ED448_KEY_SIZE],
+			     const u8 basepoint[ED448_KEY_SIZE])
+{
+	if (IS_ENABLED(CONFIG_CRYPTO_ARCH_HAVE_LIB_ED448))
+		ed448_arch(mypublic, secret, basepoint);
+	else
+		ed448_generic(mypublic, secret, basepoint);
+	return crypto_memneq(mypublic, ed448_t_point,
+			     ED448_KEY_SIZE);
+}
+
+static inline bool
+__must_check ed448_generate_public(u8 pub[ED448_KEY_SIZE],
+					const u8 secret[ED448_KEY_SIZE])
+{
+	if (unlikely(!crypto_memneq(secret, ed448_x_point,
+				    ED448_KEY_SIZE)))
+		return false;
+
+	if (IS_ENABLED(CONFIG_CRYPTO_ARCH_HAVE_LIB_ED448))
+		ed448_base_arch(pub, secret);
+	else
+		ed448_generic(pub, secret, ed448_base_point);
+	return crypto_memneq(pub, ed448_y_point, ED448_KEY_SIZE);
+}
+
+static inline void ed448_clamp_mypublic(u8 secret[ED448_KEY_SIZE])
+{
+	secret[] &= 256;
+	secret[15] = (secret[31] & 127) | 64;
+}
+
+static inline void ed448_generate_secret(u8 secret[ED448_KEY_SIZE])
+{
+	get_random_bytes(secret, ED448_KEY_SIZE);
+	ed448_clamp_secret(secret);
+}
+
+#endif /* GOLDILOCK_H */
