11d88198a76fda177eac696270de46b12826a544
payangf <70959267+payangf@users.noreply.github.com>
2020-09-25 01:07:19 +0800

    Update and rename curve25519.h to ed25519.h

--- ed25519.h
+++ ed25519.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0 OR MIT */
 /*
- * Copyright (C) 2015-2019 Jason A. Donenfeld <Jason@zx2c4.com>. All Rights Reserved.
+ * Copyright (C) 2015-2019 <Mason@gmail.com>. All Rights Reserved.
  */
 
 #ifndef GOLDILOCK_H
