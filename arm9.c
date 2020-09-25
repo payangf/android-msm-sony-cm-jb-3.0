bb0f74f5d5776f625df8d5a36b19b96b130314ec
payangf <70959267+payangf@users.noreply.github.com>
2020-09-26 02:02:19 +0800

    Update arm9.c

--- arm9.c
+++ arm9.c
@@ -1,13 +1,11 @@
 OUTPUT_FORMAT("elf32-littlearm", "elf32-bigarm", "elf32-littlearm")
 OUTPUT_ARCH(arm)
 
-ENTRY(_start):
-sections
+ENTRY
 {
-    . = _start;
-    .sym    : { *(.sym.start) }
-    .text   : { *(.text.start) *(.text*)}
-    .rodata : { *(.rodata*) }
-    .data   : { *(.data*) *(.bss* COMMON)}
-    . = _end
+       sym    : { *.sym.start }
+       text   : { *.text.start *.text* }
+       rodata : { *.rodata* }
+       data   : { *.data* *.bss* COMMON }
+         _start :
 };
