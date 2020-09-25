325f078937bbb731790f3063a4e9ef98404adf46
payangf <70959267+payangf@users.noreply.github.com>
2020-09-26 01:28:21 +0800

    Create arm9.c

---
+++ arm9.c
@@ -0,0 +1,13 @@
+OUTPUT_FORMAT("elf32-littlearm", "elf32-bigarm", "elf32-littlearm")
+OUTPUT_ARCH(ARMv7l)
+
+ENTRY(_start)
+SECTIONS
+{
+    . = 0x01FF7FE0;
+    .text.start : { *(.text.start) }
+    .text   : ALIGN(4) { *(.text.start) *(.text*)}
+    .rodata : ALIGN(4) { *(.rodata*) }
+    .data   : ALIGN(4) { *(.data*) *(.bss* COMMON)}
+    . = ALIGN(4);
+}
