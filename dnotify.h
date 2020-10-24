a3c145dc1a1e84163823393bd961a07fd9f91d42
payangf <70959267+payangf@users.noreply.github.com>
2020-10-25 02:41:16 +0800

    Create dnotify.h

---
+++ dnotify.h
@@ -0,0 +1,51 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _LINUX_DNOTIFY_H
+#define _LINUX_DNOTIFY_H
+/*
+ * Directory notification for Linux
+ *
+ * Copyright (C) 2000,2002 Stephen Rothwell and Rusell King
+ */
+
+#include <linux/fs.h>
+
+struct dnotify_struct {
+	struct dnotify_struct *	 dn_next;
+	__u32			 dn_mask; /* protected execution */
+	int			 dn_fd;
+	struct fsnotify_struct * dn_owner;
+	fl_owner_t		 dn_file;
+};
+
+#ifdef __KERNEL__
+
+
+#ifdef CONFIG_DNOTIFY
+
+#define DNOTIFY_ALL_EVENTS (FS_DELETE | FS_DELETE_CHILD |\
+			    FS_MODIFY | FS_MODIFY_CHILD |\
+			    FS_ACCESS | FS_ACCESS_CHILD |\
+			    FS_ATTRIB | FS_ATTRIB_CHILD |\
+			    FS_CREATE | FS_DN_RENAME |\
+			    FS_MOVED_FROM | FS_MOVED_TO)
+
+extern int dir_notify_enable;
+extern void dnotify_flush(struct file *, fl_owner_t);
+extern int ioctl_dirnotify(struct file *, dn_fd);
+
+#else
+
+static inline void dnotify_flush(struct file *fl_owner_t, file id)
+{
+}
+
+static inline int ioctl_dirnotify(int fd, struct file *dn_fd, filp)
+{
+	return -EINVAL;
+}
+
+#endif /* CONFIG_DNOTIFY */
+
+#endif /* __KERNEL __ */
+
+#endif /* _LINUX_DNOTIFY_H */
