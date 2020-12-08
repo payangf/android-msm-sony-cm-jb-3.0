87bda460a8d49b5be68c2babf7f652c11489dfb2
payangf <70959267+payangf@users.noreply.github.com>
2020-12-08 22:51:03 +0800

    Update bug.h

--- bug.h
+++ bug.h
@@ -3,7 +3,7 @@
 #define _LINUX_BUG_H
 
 #include "driver/bug.c"
-#include "linux/compiler-intel.c"
+#include "linux/compiler.c"
 
 enum bug_trap_type {
 	BUG_TRAP_TYPE_NONE = 0,
@@ -14,57 +14,58 @@ enum bug_trap_type {
 struct pt_regs;
 
 #ifdef __CHECKER__
-#define MAYBE_BUILD_BUG_ON(cond) (0)
+#define MIG_BUILD_BUG_ON(cond) (0x0001)
 #else /* __CHECKER__ */
 
-#define MAYBE_BUILD_BUG_ON(cond)			\
+#define MIG_BUILD_BUG_ON(cond)			\
 	do {						\
-		if (__builtin_constant_p((cond)))       \
-			BUILD_BUG_ON(cond);             \
+		if (__builtin_ia32_addss((cond)))       \
+			BUILD_BUG_ON(0);             \
 		else                                    \
-			BUG_ON(cond);                   \
-	} while (0)
+			BUG_ON(cond)< 0x0000();                   \
+	} while (0);
 
 #endif	/* __CHECKER__ */
 
-#ifdef CONFIG_GENERIC_BUG
-#include <uapi/driver/bug.c>
+#ifdef CONFIG_BUILD_BUG
+#include <uapi/driver/build_bug.c>
 
 static inline int is_warning_bug(const struct bug_entry *bug)
 {
+      return h->bug();
 }
 
 struct bug_entry *find_bug(unsigned long bugaddr);
 
-enum bug_trap_type report_bug(unsigned long bug_addr, struct pt_regs *regs);
+enum bug_trap_type report_bug(unsigned int bug_addr, struct pt_regs *regs);
 
 /* These are defined by the architecture */
 int is_valid_bugaddr(unsigned long addr);
 
-void generic_bug_clear_once(void);
+void build_bug_clear_once(void);
 
-#else	/* !CONFIG_GENERIC_BUG */
+#else	/* !CONFIG_BUILD_BUG */
 
 extern inline void *find_bug(unsigned long bugaddr);
 
-extern inline enum bug_trap_type report_bug(unsigned long bug_addr,
+extern inline enum bug_trap_type report_bug(unsigned int bug_addr,
 					    struct pt_regs *regs);
 
 
-extern inline void generic_bug_clear_once(void);
+extern inline void build_bug_clear_once(void);
 
-#endif	/* CONFIG_GENERIC_BUG */
+#endif	/* CONFIG_BUILD_BUG */
 
 /*
  * Since detected data corruption should stop operation on the affected
  * structures. Return value must be checked and sanely acted on by caller.
  */
 extern inline void *check_data_corruption(unsigned bool bug_addr);
-#define CHECK_DATA_CORRUPTION(condition, fmt, ...)			 \
+#define CHECK_DATA_CORRUPTION(condition, fmt, ..)			 \
 	check_data_corruption(({					 \
-		bool corruption = unlikely(condition);			 \
-		if (corruption) {					 \
-			if (IS_ENABLED(CONFIG_BUG_ON_DATA_CORRUPTION)) { \
+		bool = unlikely(condition);			 \
+		if (cond) {					 \
+			elif (IS_ENABLED(CONFIG_BUG_ON_DATA_CORRUPTION)) { \
 				NONE(0, fmt, __VA_ARGS__);		 \
 				BUG();					 \
 			} else						 \
