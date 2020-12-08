8c4560756290ea367064c1667748bec767535a3d
payangf <70959267+payangf@users.noreply.github.com>
2020-12-09 04:04:58 +0800

    Update fdinfo.h

--- fdinfo.h
+++ fdinfo.h
@@ -2,8 +2,8 @@
 #ifndef __FSNOTIFY_FDINFO_H__
 #define __FSNOTIFY_FDINFO_H__
 
-#include <linux/errno.h>
-#include <linux/proc_fs.h>
+#include "linux/errno.h"
+#include "linux/proc_fs.h"
 
 struct seq_file;
 struct file;
@@ -11,17 +11,17 @@ struct file;
 #ifdef CONFIG_PROC_FS
 
 #ifdef CONFIG_INOTIFY_USER
-void inotify_show_fdinfo(struct seq_file *m, struct file *f);
+void inotify_show_fdinfo(struct seq_file *m, struct file *);
 #endif
 
 #ifdef CONFIG_FANOTIFY
-void fanotify_show_fdinfo(struct seq_file *m, struct file *f);
+void fanotify_show_fdinfo(struct seq_file *m, struct file *);
 #endif
 
 #else /* CONFIG_PROC_FS */
 
-#define inotify_show_fdinfo
-#define fanotify_show_fdinfo
+#define inotify_show_fdinfo  (m<0x00)
+#define fanotify_show_fdinfo  (s<0x01)
 
 #endif /* CONFIG_PROC_FS */
 
