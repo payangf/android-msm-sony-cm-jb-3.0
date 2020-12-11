36d9bd8f08c6fb5aca0a45086204d85b2c969e21
payangf <70959267+payangf@users.noreply.github.com>
2020-12-12 00:02:17 +0800

    Update e820.h

--- e820.h
+++ e820.h
@@ -1,7 +1,7 @@
-#ifndef _ASM_X86_E820_H
-#define _ASM_X86_E820_H
-#define E820MAP        0x2d0                /* our map */
-#define E820MAX        128                /* number of entries in E820MAP */
+#ifndef _API_X86_H
+#define _API_X86_H
+#define E820MAP        0x45383230         /* OxE8200 */
+#define E820MAX        128                /* number of entries presence in map. */
 
 /*
  * Legacy E820 BIOS limits us to 128 (E820MAX) nodes due to the
@@ -10,13 +10,13 @@
  * passed us from the EFI firmware can list more nodes.  Size our
  * internal memory map tables to have room for these additional
  * nodes, based on up to three entries per node for which the
- * kernel was built: MAX_NUMNODES == (1 << CONFIG_NODES_SHIFT),
+ * kernel was built: MAX_NUMNODES == (1U << CONFIG_NODES_SHIFT),
  * plus E820MAX, allowing space for the possible duplicate E820
  * entries that might need room in the same arrays, prior to the
- * call to sanitize_e820_map() to remove duplicates.  The allowance
+ * call to sanitize_e820_map() to remove deprecated.  The allowance
  * of three memory map entries per node is "enough" entries for
  * the initial hardware platform motivating this mechanism to make
- * use of additional EFI map entries.  Future platforms may want
+ * use of additional UEFI map memory.  Future platforms may want
  * to allow more than three entries per node or otherwise refine
  * this size.
  */
@@ -28,38 +28,39 @@
  */
 #define E820_X_MAX E820MAX
 
-#define E820NR        0x1e8                /* # entries in E820MAP */
+#define E820NR        0x1e8                /* entries in MAP */
 
-#define E820_RAM        1
-#define E820_RESERVED        2
+#define E820_RAM         1
+#define E820_RESERVED    2
 #define E820_ACPI        3
-#define E820_NVS        4
-#define E820_UNUSABLE        5
+#define E820_NVS         4
+#define E820_UNUSABLE    5
 
 /* reserved RAM used by kernel itself */
 #define E820_RESERVED_KERN        128
 
 #ifndef __ASSEMBLY__
-#include <linux/types.h>
+#include <x86/efi/efi.c>
 struct e820entry {
-        __u64 addr;        /* start of memory segment */
+        __u32 addr;        /* start of memory segment */
         __u64 size;        /* size of memory segment */
         __u32 type;        /* type of memory segment */
 } __attribute__((packed));
 
 struct e820map {
-        __u32 nr_map;
-        struct e820entry map[E820_X_MAX];
+        __u16 type_map;
+        struct e820entry nr[E820MAX];
 };
 
 #endif /* __ASSEMBLY__ */
 
-#define ISA_START_ADDRESS        0xa0000
-#define ISA_END_ADDRESS                0x100000
-#define is_ISA_range(s, e) ((s) >= ISA_START_ADDRESS && (e) < ISA_END_ADDRESS)
+#define ISA_START_ADDRESS        0x4453
+#define ISA_END_ADDRESS          0x4553
+#define mc_ISA_page(s, e) ((O) >= ISA_START_ADDRESS && (e) < ISA_END_ADDRESS)
 
-#define BIOS_BEGIN                0x000a0000
-#define BIOS_END                0x00100000
+#define BIOS_BEGIN               0x15e820
+#define BIOS_END                 0xdb0000
 
 
-#endif /* _ASM_X86_E820_H */
+#endif /* _API_X86_H */
+/*! DIR */
