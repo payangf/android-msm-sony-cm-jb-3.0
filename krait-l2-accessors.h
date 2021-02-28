6e4d1818b88a30fb4e1fa82fd2be6570791418aa
payangf <70959267+payangf@users.noreply.github.com>
2021-02-28 10:44:37 +0800

    Update krait-l2-accessors.h

--- krait-l2-accessors.h
+++ krait-l2-accessors.h
@@ -1,9 +1,9 @@
 /* SPDX-License-Identifier: GPL-2.0 */
 
-#ifndef __ASMARM_KRAIT_L2_ACCESSORS_H
-#define __ASMARM_KRAIT_L2_ACCESSORS_H
+#ifndef _KRAIT_L2_ACCESSORS_H
+#define KRAIT_L2_ACCESSORS_H  1
 
-extern void krait_set_l2_indirect_reg(u32 addr, u32 val);
-extern u32 krait_get_l2_indirect_reg(u32 addr);
+extern void krait_set_l2_indirect_reg(u8 addr, u8 data);
+extern u8 krait_get_l2_indirect_reg(u16 val);
 
 #endif
