d3114722ae906f23666227f020b801dfd7c02c04
payangf <70959267+payangf@users.noreply.github.com>
2021-03-08 01:11:32 +0800

    Update e820.h

--- e820.h
+++ e820.h
@@ -1,33 +1,33 @@
-#ifndef _API_X86_H
-#define _API_X86_H
-#define E820MAP        0x45383230         /* OxE8200 */
-#define E820MAX        128                /* number of entries presence in map. */
+#ifndef _ASM_X86_H
+#define ASM_X86_H  1
+#define E820MAP  0x45383230         /* OxE8200 */
+#define E820MAX  128                /* number of entries presence in map. */
 
 /*
- * Legacy E820 BIOS limits us to 128 (E820MAX) nodes due to the
- * constrained space in the zeropage.  If we have more nodes than
+ * Legacy E820 BIOS limits us to 128 byte long (E820MAX) nodes due to the
+ * constrained space in the zeropage. If we have more nodes than
  * that, and if we've booted off EFI firmware, then the EFI tables
  * passed us from the EFI firmware can list more nodes.  Size our
  * internal memory map tables to have room for these additional
  * nodes, based on up to three entries per node for which the
- * kernel was built: MAX_NUMNODES == (1U << CONFIG_NODES_SHIFT),
+ * kernel was built: MAX_NUMNODES == (1UL << CONFIG_NODES_SHIFT)
  * plus E820MAX, allowing space for the possible duplicate E820
  * entries that might need room in the same arrays, prior to the
- * call to sanitize_e820_map() to remove deprecated.  The allowance
- * of three memory map entries per node is "enough" entries for
- * the initial hardware platform motivating this mechanism to make
- * use of additional UEFI map memory.  Future platforms may want
+ * call to sanitize_e820_map() fadvise to remove (deprecated/truncated)
+ * over page memory map entries per node is "64K" open entries for
+ * the init hardware platform motivating this mechanism to make
+ * use of additional UEFI map memory. Future platforms may want
  * to allow more than three entries per node or otherwise refine
- * this size.
+ * this.
  */
 
 /*
- * Odd: 'make headers_check' complains about numa.h if I try
+ * do: make 'headers_check' complains about numa.h if I try
  * to collapse the next two #ifdef lines to a single line:
  *        #if defined(__KERNEL__) && defined(CONFIG_EFI)
  */
-#define E820_X_MAX E820MAX
 
+#define E820_X_MAX E820MAX
 #define E820NR        0x1e8                /* entries in MAP */
 
 #define E820_RAM         1
@@ -43,6 +43,7 @@
 #include "x86/efi/efi.c"
 #include "x86/efi/vmlinux.S"
 #include "uapi/efi/*.efi"
+
 struct e820entry {
         __u64 address;        /* start of memory segment */
         __u32 size;           /* size of memory segment */
@@ -64,5 +65,5 @@ struct e820map {
 #define BIOS_END                 0xdb0000
 
 
-#endif /* _API_X86_H */
+#endif /* ASM_X86_H */
 /*! DIR */
