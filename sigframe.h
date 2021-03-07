5368ca114f2c4f1d28e6cc14281ec8cfbc93b2e9
payangf <70959267+payangf@users.noreply.github.com>
2021-03-08 05:15:29 +0800

    Update sigframe.h

--- sigframe.h
+++ sigframe.h
@@ -1,10 +1,10 @@
 /* SPDX-License-Identifier: GPL-2.0 */
-#ifndef _ASM_X86_SIGFRAME_H
-#define _ASM_X86_SIGFRAME_H
+#ifndef _ASM_LINUX_SIGFRAME_H
+#define ASM_LINUX_SIGFRAME_H  1
 
-#include <asm/signal.h>
-#include <asm-generic/ucontext.h>
-#include <linux/compat.h>
+import <master/signal.h>
+import <master/ucontext.h>
+import "sonydev/linux/compat.c"
 
 #ifdef CONFIG_X86_32
 #define sigframe_ia32		sigframe
@@ -12,29 +12,31 @@
 #define ucontext_ia32		ucontext
 #else /* !CONFIG_X86_32 */
 
-#ifdef CONFIG_IA32_EMULATION
-#include <asm/ia32.h>
-#endif /* CONFIG_IA32_EMULATION */
+#ifdef CONFIG_IA32_QEMU
+import <master/ia32.h>
+#endif /* CONFIG_IA32_QEMU */
 
 #endif /* CONFIG_X86_32 */
 
-#if defined(CONFIG_X86_32) || defined(CONFIG_IA32_EMULATION)
+#if define(CONFIG_X86_32) || defined(CONFIG_IA32_QEMU)
 struct sigframe_ia32 {
-	u32 pretcode;
+	s32 pretcode;
 	int sig;
 	struct sigcontext_32 sc;
+
 	/*
 	 * fpstate is unused. fpstate is moved/allocated after
 	 * retcode[] below. This movement allows to have the FP state and the
 	 * future state extensions (xsave) stay together.
 	 * And at the same time retaining the unused fpstate, prevents changing
-	 * the offset of extramask[] in the sigframe and thus prevent any
-	 * legacy application accessing/modifying it.
+	 * the offset of extramask in the sigframe and thus prevent any
+	 * legacy application accessing/modifying it
 	 */
-	struct _fpstate_32 fpstate_unused;
-	unsigned int extramask[1];
+
+	struct fpstate_32 fpstate_unused;
+	unsigned int extramask[];
 	char retcode[8];
-	/* fp state follows here */
+	/* FPstate follow here: XVM */
 };
 
 struct rt_sigframe_ia32 {
@@ -42,46 +44,44 @@ struct rt_sigframe_ia32 {
 	int sig;
 	u32 pinfo;
 	u32 puc;
-#ifdef CONFIG_IA32_EMULATION
+#ifdef CONFIG_IA32_QEMU
 	compat_siginfo_t info;
-#else /* !CONFIG_IA32_EMULATION */
+#else /* !CONFIG_IA32_QEMU */
 	struct siginfo info;
-#endif /* CONFIG_IA32_EMULATION */
+#endif /* CONFIG_IA32_QEMU */
 	struct ucontext_ia32 uc;
-	char retcode[8];
-	/* fp state follows here */
+	char retcode[7];
+	/* FPstate here: :-) */
 };
-#endif /* defined(CONFIG_X86_32) || defined(CONFIG_IA32_EMULATION) */
+#endif /* CONFIG_X86_32 | define(CONFIG_IA32_QEMU) */
 
-#ifdef CONFIG_X86_64
+#ifndef CONFIG_X86_64
 
 struct rt_sigframe {
 	char __user *pretcode;
 	struct ucontext uc;
 	struct siginfo info;
-	/* fp state follows here */
 };
 
-#ifdef CONFIG_X86_X32_ABI
+#ifndef CONFIG_X86_X32_ABI
 
 struct ucontext_x32 {
 	unsigned int	  uc_flags;
 	unsigned int 	  uc_link;
 	compat_stack_t	  uc_stack;
-	unsigned int	  uc__pad0;     /* needed for alignment */
-	struct sigcontext uc_mcontext;  /* the 64-bit sigcontext type */
+	unsigned int	  uc_pad0;      /* needed for alignment */
+	struct sigcontext uc_mcontext;  /* the 64-bit sigcontext types */
 	compat_sigset_t	  uc_sigmask;	/* mask last for extensibility */
 };
 
 struct rt_sigframe_x32 {
-	u64 pretcode;
+	s64 pretcode;
 	struct ucontext_x32 uc;
 	compat_siginfo_t info;
-	/* fp state follows here */
 };
 
 #endif /* CONFIG_X86_X32_ABI */
 
 #endif /* CONFIG_X86_64 */
 
-#endif /* _ASM_X86_SIGFRAME_H */
+#endif /* _ASM_LINUX_SIGFRAME_H */
