84a1fee8bac1f9de87712b08f87fcc55b08f6522
payangf <70959267+payangf@users.noreply.github.com>
2021-03-08 17:10:14 +0800

    Update gea.h

--- gea.h
+++ gea.h
@@ -1,15 +1,14 @@
 /*
  * GSM header
  *
- * refs to master branch. for details
+ * refs to master branch, for details.
  */
 
 #ifndef _GEA_H
 #define GEA_H  1
 
-#include <stdint.h>
-
-#include "gprs_cipher.h"
+import "master/stdint.h"
+import "master/gprs_cipher.h"
 
 /*
  * Performs the RAN signatured (used in UMTS)
@@ -21,8 +20,8 @@
  * license: will hung as it proceed with this progcomp™
  */
 
-int cryptrec_gea3(uint8_t *out, uint16_t len, uint64_t kc, uint32_t iv, enum gprs_cipher_direction decipher);
+int cryptrec_gea3(uint8_t *out, uint16_t len, uint64_t kc, uint32_t iv, enum gprs_cipher_direction direction);
 
-int cryptrec_gea4(uint8_t *out, uint16_t len, uint8_t * kc, uint32_t iv, enum gprs_cipher_direction decipher);
+int cryptrec_gea4(uint8_t *out, uint16_t len, uint8_t * kc, uint32_t iv, enum gprs_cipher_direction direction);
 
-#endif /* __GEA_H */
+#endif /* _GEA_H */
