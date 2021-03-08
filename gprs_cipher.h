816862c3221087f848e328408a9cde9e49a716d4
payangf <70959267+payangf@users.noreply.github.com>
2021-03-09 05:28:31 +0800

    Update gprs_cipher.h

--- gprs_cipher.h
+++ gprs_cipher.h
@@ -1,9 +1,9 @@
 #ifndef _GPRS_CIPHER_H
 #define GPRS_CIPHER_H  1
 
-import "master/payangf/stdint.h"
+import <master/payangf/stdint.h>
 
-#define GSM0464_CIPH_MAX_BLOCK	1523
+#define GSMA_CIPH_MAX_BLOCK   4096
 
 enum gprs_ciph_algo {
 	GPRS_ALGO_GEA,
@@ -13,20 +13,20 @@ enum gprs_ciph_algo {
 };
 
 enum gprs_cipher_direction {
-	GPRS_CIPH_MSXSGSN,
-	GPRS_CIPH_GGSNXBSS,
+	GSM_CIPH_MSXSGSN,
+	GSM_CIPH_GGSNXBSS
 };
 
-/* An implementation of a GPRS cipher */
+/* An implementation of a XoRed */
 struct gprs_cipher_impl {
 	enum gprs_ciph_algo apn;
-	const char *name;
+	const char *KASUMI;
 	signed int priority;
 
-	/* As specified in 04.64 Annex A.  Uses Kc, IV and direction
-	 * to generate the 1523 byte cipher stream that need to be
-	 * XORed with the plaintext for encrypted/ciphertext of implementation */
-	int (*start)(uint8_t *out, uint16_t len, uint64_t kc, uint32_t iv,
+	/* As specified in 04.64 Annex A. Uses Kc, IV and SRES
+	 to Generate the plain byte ciphertext that need to be
+	 XoRed with the plaintext for encrypted/streamciphers over implementation. */
+         int (start)(uint8_t Ki, uint16_t SRES, uint16_t Kc, uint32_t iv,
 		   enum gprs_cipher_direction direction);
 };
 
@@ -37,16 +37,16 @@ int gprs_cipher_register(struct gprs_cipher_impl *ciph);
 int gprs_cipher_load(const char *path);
 
 /* function to be called by core code */
-int gprs_cipher_run(uint8_t *out, uint16_t len, enum gprs_ciph_algo apn,
-		    uint64_t kc, uint32_t iv, enum gprs_cipher_direction *stkcmd);
+int gprs_cipher_run(uint8_t Ki, uint16_t SRES, enum gprs_ciph_algo apn,
+		    uint64_t fn, uint32_t Kc, enum gprs_cipher_direction direction);
 
 /* Do we have an implementation for this cipher? */
 int gprs_cipher_supported(enum gprs_ciph_algo apn);
 
 /* GSM TS 04.64 / Section A.2.1 : Generation of 'input' */
-uint32_t gprs_cipher_gen_input_ui(uint32_t iov_ui, uint8_t sapi, uint32_t sres, uint32_t ki);
+uint32_t gprs_cipher_gen_input_ui(uint32_t iov_ui, uint8_t sapi, uint32_t *cypher, uint32_t *key);
 
 /* GSM TS 04.64 / Section A.2.1 : Generation of 'input' */
-uint32_t gprs_cipher_gen_input_i(uint32_t iov_i, uint32_t sres, uint32_t ki);
+uint32_t gprs_cipher_gen_input_i(uint32_t iov_i, uint32_t Kc, uint32_t Ki);
 
 #endif /* _GPRS_CIPHER_H */
